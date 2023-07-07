1976ddce4339a28f3dd168bbd1b42d4c4ba4d395
lerkooow <lerkoooow@gmail.com>
2023-07-07 19:51:23 +0530

    Сделала заголовок Specials и кнопку

--- src/Main.js
+++ src/Main.js
@@ -14,8 +14,17 @@ export default function Main() {
                             <h3 className="intro_info">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                 sed do eiusmod tempor incididunt
                                 ut labore et dolore magna aliqua.</h3>
+                            <input className="intro_b" type="button" value="Reserve a Table"></input>
                         </div>
                         <img className="img_intro" src={restauranfood} alt="img_intro"></img>
+                    </div>
+                </div>
+            </section>
+            <section className="specials">
+                <div className="wrapper">
+                    <div className="specials_wrapper">
+                        <h2 className="h2_specials">Specials</h2>
+                        <input className="specials_b" type="button" value="Online Menu"></input>
                     </div>
                 </div>
             </section>
