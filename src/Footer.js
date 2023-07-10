e2215e0df25d665a87dc21186f43b233dd092530
lerkooow <lerkoooow@gmail.com>
2023-07-10 17:25:09 +0530

    Сделала footer

--- src/Footer.js
+++ src/Footer.js
@@ -17,19 +17,27 @@ export default function Footer() {
     return (
         <footer footer className="footer">
             <div className="wrapper">
-                <img className="footer-restaurant" src={restaurant} alt="restaurant"></img>
-                <h3 className="h3_navigation">Doormat Navigation</h3>
-                <ul className="navigation_list">
-                    {footerNavigation}
-                </ul>
-                <h3 className="h3_navigation">Contact</h3>
-                <ul className="navigation_list">
-                    {footerContactLink}
-                </ul>
-                <h3 className="h3_navigation">Social Media Links</h3>
-                <ul className="navigation_list">
-                    {footerContactLink}
-                </ul>
+                <div className="footer_wrapper">
+                    <img className="img_footer" src={restaurant} alt="restaurant"></img>
+                    <div className="footer_navigation">
+                        <h3 className="h3_navigation">Doormat Navigation</h3>
+                        <ul className="navigation_list">
+                            {footerNavigation}
+                        </ul>
+                    </div>
+                    <div className="footer_contact">
+                        <h3 className="h3_navigation">Contact</h3>
+                        <ul className="navigation_list">
+                            {footerContactLink}
+                        </ul>
+                    </div>
+                    <div className="footer_link">
+                        <h3 className="h3_navigation">Social Media Links</h3>
+                        <ul className="navigation_list">
+                            {footerContactLink}
+                        </ul>
+                    </div>
+                </div>
             </div>
 
         </footer>
